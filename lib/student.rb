2d4b6d95824578ba9eb0a71d2f6cd3c55dd58816
Sharon Watkins <swatkins876@gmail.com>
2019-11-11 17:35:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXIST students(
+    CREATE TABLE IF NOT EXIST students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
