eb013fc779551dfcc4868bd75c3fcf561498903e
Sharon Watkins <swatkins876@gmail.com>
2019-11-11 17:27:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,11 @@ class Student
 
   attr_writer :id
 
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
